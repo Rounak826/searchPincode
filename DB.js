8d16eec39e6e94cb7ac509b8154fca1c35a8f46c
rounak bisen <r26bisen@gmail.com>
2024-02-03 19:43:29 +0530

    manual pg installation

--- DB.js
+++ DB.js
@@ -1,9 +1,11 @@
 // database/connection.js
 const { Sequelize } = require('sequelize');
+const pg = require('pg')
 require('dotenv').config()
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  dialectModule: pg,
   ssl: true,
   dialectOptions: {
     ssl: {
